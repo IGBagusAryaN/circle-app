d0969a3ea1d810e907ca27c8c7acd3e3655a72c2
Bagusarya <aryabagus453@gmail.com>
2025-09-07 15:51:06 +0700

    new

--- src/socket.ts
+++ src/socket.ts
@@ -1,18 +1,18 @@
-import io from 'socket.io-client';
-import { useAuthStore } from 'store/use.auth.store';
+// import io from 'socket.io-client';
+// import { useAuthStore } from 'store/use.auth.store';
 
-export const socket = io('http://localhost:3000');
+// export const socket = io('http://localhost:3000');
 
-socket.on('connect', () => {
-  console.log('✅ Connected with ID:', socket.id);
-  const userId = useAuthStore.getState().user?.id;
-  console.log('🧾 Current userId:', userId);
+// socket.on('connect', () => {
+//   console.log('✅ Connected with ID:', socket.id);
+//   const userId = useAuthStore.getState().user?.id;
+//   console.log('🧾 Current userId:', userId);
 
-  if (userId) {
-    const room = `user-${userId}`;
-    console.log('📡 Emit joinRoom dengan:', room);
-    socket.emit('joinRoom', room);
-  }
-});
+//   if (userId) {
+//     const room = `user-${userId}`;
+//     console.log('📡 Emit joinRoom dengan:', room);
+//     socket.emit('joinRoom', room);
+//   }
+// });
 
 
